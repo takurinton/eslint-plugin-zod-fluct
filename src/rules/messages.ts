32cbfc285c542ccbe6f96afbcb898488eb29b036
takurinton <takurinton@takurinton.dev>
2023-02-27 06:00:40 +0900

    fix

--- src/rules/messages.ts
+++ src/rules/messages.ts
@@ -9,7 +9,10 @@ export const messages: Messages = {
   not_max_error: "{{ name }}を使用しているときはmax()を必ず指定してください",
   not_min_error_message: "min()のエラーメッセージを指定してください",
   not_max_error_message: "max()のエラーメッセージを指定してください",
-  error_message_must_be_object: "エラーメッセージは文字列で指定してください",
+
+  // ref: https://github.com/colinhacks/zod/issues/97#issuecomment-664178323
+  error_message_must_be_object:
+    "エラーメッセージはオブジェクトで指定してください",
   string_must_have_min_if_not_optional:
     "z.string()はoptional()を使用していない場合はmin()を必ず指定してください",
   nullable_and_optional_must_have_invalid_type_error:
