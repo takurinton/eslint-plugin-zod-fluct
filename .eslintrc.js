9c31b8d469b89e2d5d50e308f0f9e6566cda4442
takurinton <takurinton@takurinton.dev>
2023-04-19 08:26:20 +0900

    fix: settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["local-rules", "@typescript-eslint", "local-rules"],
+  plugins: ["local-rules", "@typescript-eslint"],
   rules: {
     semi: ["error", "always"],
     "no-extra-semi": "error",
