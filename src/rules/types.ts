6b31586a45ef63876bd6e73863b5480c1348505f
takurinton <takurinton@takurinton.dev>
2023-04-21 20:03:29 +0900

    remove unused

--- src/rules/types.ts
+++ src/rules/types.ts
@@ -1,15 +1,10 @@
 export type Errors =
-  | "required_error"
-  | "invalid_type_error"
   | "not_min_error"
-  | "do_not_use_other_than_min_and_max_if_number"
   | "not_max_error"
   | "not_min_error_message"
   | "not_max_error_message"
   | "error_message_must_be_object"
   | "string_must_have_min_if_not_nullable"
-  | "nullable_and_optional_must_have_invalid_type_error"
-  | "not_nullable_and_not_optional_must_have_required_error"
   | "not_use_alias"
   | "not_use_method";
 
